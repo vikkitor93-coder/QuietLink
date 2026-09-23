edca3037869ee0338f3906b3141aa78fe000a905
vikkitor93-coder <vikkitor93@gmail.com>
2026-09-23 21:24:28 +0000

    Set Android v0.3.62 build identity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,8 +8,8 @@ android {
         applicationId = "is.quietlink.app"
         minSdk = 28
         targetSdk = 36
-        versionCode = 73
-        versionName = "0.3.61"
+        versionCode = 74
+        versionName = "0.3.62"
     }
 
     buildTypes {
